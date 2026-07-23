b270937f81ba26248a386cf13dc6078119caab58
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-07-23 17:47:11 +0530

    Fix backend url3

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smarttime-backend-8.onrender.com/api/auth",
+  baseURL: "https://smarttime-backend-10.onrender.com/api/auth",
 });
 
 // Register User
--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smarttime-backend-8.onrender.com/api/tasks",
+  baseURL: "https://smarttime-backend-10.onrender.com/api/tasks",
 });
 
 const getToken = () => {
